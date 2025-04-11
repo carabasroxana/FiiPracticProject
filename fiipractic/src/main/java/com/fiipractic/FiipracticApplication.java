c9d276d061daa6c3d070a5e177321f0e64caa387
carabasroxana <carabasroxana@yahoo.com>
2025-04-11 18:53:13 +0300

    Add user, profile, and request history controllers, entities, and services

--- fiipractic/src/main/java/com/fiipractic/FiipracticApplication.java
+++ fiipractic/src/main/java/com/fiipractic/FiipracticApplication.java
@@ -1,7 +1,9 @@
 package com.fiipractic;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestClient;
 
 @SpringBootApplication
@@ -10,4 +12,15 @@ public class FiipracticApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FiipracticApplication.class, args);
 	}
-}
+
+	@Bean
+	public CommandLineRunner dataLoader(UserService userService) {
+		return args -> {
+			if (userService.getUserByUsername("admin") == null) {
+				UserEntity.User admin = new UserEntity.User("admin", "admin123");
+				userService.saveUser(admin);
+				System.out.println("Inserted default admin user");
+			}
+		};
+	}
+	}
